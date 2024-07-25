550a0fa9dc2cc966eb027b1eeac87823fb3be8b4
minimuscle <joshthiele@live.com.au>
2024-07-25 20:36:07 +1000

    basic navigation

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,25 @@
-import { MantineProvider } from '@mantine/core'
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import '@mantine/tiptap/styles.css'
-import '@mantine/core/styles.css'
+import { Button, MantineProvider } from "@mantine/core"
+import {
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react"
+import "@mantine/tiptap/styles.css"
+import "@mantine/core/styles.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Root Header</h1>
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -25,6 +31,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <MantineProvider>
+      <Button component={Link} to={"/"}>
+        Home
+      </Button>
+      <h1>Root Header</h1>
       <Outlet />
     </MantineProvider>
   )
